9f8ab467d347263468d76d9a60bde8a64d4ad949
Kaushal Raj Mishra <kaushu42@gmail.com>
2017-12-18 20:16:35 +0545

    Added files

---
+++ run.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv){
+  string filename(argv[1]);
+  string compile = "avr-gcc -g -Os -mmcu=atmega32 -c " + filename + ".cpp";
+  system(compile.c_str());
+  cout << compile << endl;
+  compile = "avr-gcc -g -mmcu=atmega32 -o " + filename + ".elf " + filename + ".o";
+  system(compile.c_str());
+  cout << compile << endl;
+  compile = "avr-objcopy -j .text -j .data -O ihex " + filename + ".elf " + filename + ".hex";
+  system(compile.c_str());
+  cout << compile << endl;
+
+}
